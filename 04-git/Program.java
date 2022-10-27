a3bb4c4873d331914a48dd14763d68f3bfaa7a29
lewuss <zenek.zupa@gmail.com>
2022-10-27 13:41:54 +0200

    changed code

--- 04-git/Program.java
+++ 04-git/Program.java
@@ -16,8 +16,8 @@ class GFG {
             // Swap
             int num3 = num2 + num1;
             num1 = num2;
-            num2 = num3;
-            counter = counter + 1;
+            num2 = num3+3;
+            counter = counter + 2;
         }
     }
   
